d240180862cad58a41afff8a0a08205f27875851
Robert <robertslootjes@gmail.com>
2016-04-22 11:47:04 +0200

    - Removed unused code

--- Schema/DblibSchemaManager.php
+++ Schema/DblibSchemaManager.php
@@ -35,41 +35,4 @@ class DblibSchemaManager extends SQLServerSchemaManager
 
         return array_map([$this->_conn->formatter, 'fixSequenceName'], $tableNames);
     }
-
-    /**
-     * lists table views
-     *
-     * @param string $table database table name
-     * @return array
-     */
-    public function listTableViews($table)
-    {
-        $keyName = 'INDEX_NAME';
-        $pkName  = 'PK_NAME';
-        if ($this->_conn->getAttribute(Doctrine::ATTR_PORTABILITY) & Doctrine::PORTABILITY_FIX_CASE) {
-            if ($this->_conn->getAttribute(Doctrine::ATTR_FIELD_CASE) == CASE_LOWER) {
-                $keyName = strtolower($keyName);
-                $pkName  = strtolower($pkName);
-            } else {
-                $keyName = strtoupper($keyName);
-                $pkName  = strtoupper($pkName);
-            }
-        }
-        $table   = $this->_conn->quote($table, 'text');
-        $query   = 'EXEC sp_statistics @table_name = ' . $table;
-        $indexes = $this->_conn->fetchColumn($query, $keyName);
-
-        $query = 'EXEC sp_pkeys @table_name = ' . $table;
-        $pkAll = $this->_conn->fetchColumn($query, $pkName);
-
-        $result = [];
-
-        foreach ($indexes as $index) {
-            if (!in_array($index, $pkAll) && $index != null) {
-                $result[] = $this->_conn->formatter->fixIndexName($index);
-            }
-        }
-
-        return $result;
-    }
 }
